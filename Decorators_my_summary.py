aef916cc9894d73a05f4410920832657ebe6dfcb
ervand7 <ervand7@mail.ru>
2020-12-15 20:33:58 +0300

    adding 2 teachers decorators

--- Decorators_my_summary.py
+++ Decorators_my_summary.py
@@ -236,3 +236,55 @@ def multiplier_(a, b):
 # y = multiplier_(1, 0)
 # print(y)
 # print(x)
+# _________________________________________________________________________
+# 2 decorators from teacher
+
+import datetime
+import logging
+from logging.handlers import RotatingFileHandler
+from typing import Callable, Any
+
+
+def make_trace(path: str) -> Callable:
+    """Вариант с записью в файл"""
+
+    def trace(old_function: Callable) -> Callable:
+        def new_function(*args, **kwargs) -> Any:
+            result = old_function(*args, **kwargs)
+
+            with open(path, 'a') as log:
+                log.write(f'\n{datetime.datetime.utcnow()}: called {old_function.__name__}\n'
+                          f'\t args: {args}\n'
+                          f'\t kwargs: {kwargs}\n'
+                          f'\t result: {result}\n')
+
+            return result
+
+        return new_function
+
+    return trace
+
+
+def make_log(path: str) -> Callable:
+    """Вариант с использованием logging"""
+
+    def log(old_function: Callable) -> Callable:
+        logger = logging.getLogger(path)
+        logger.setLevel(logging.INFO)
+        handler = RotatingFileHandler(path, backupCount=10, maxBytes=1000000)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        def new_function(*args, **kwargs) -> Any:
+            result = old_function(*args, **kwargs)
+
+            logger.info(f'called: {old_function.__name__}\n'
+                        f'\t args: {args}\n'
+                        f'\t kwargs: {kwargs}\n'
+                        f'\t result: {result}\n')
+            return result
+
+        return new_function
+
+    return log
